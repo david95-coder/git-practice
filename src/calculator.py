e9c715a77f0218de2edf7b5f38cc56ec0018866b
David Guerrero Prada <dguerreroprada@gmail.com>
2026-09-22 01:35:50 +0200

    Add Calculator

---
+++ src/calculator.py
@@ -0,0 +1,10 @@
+type = input("Select the kind of operation you want to perform: ")
+n1 = input("Introduce the first number: ")
+n2 = input("Introduce the second number: ")
+if type == "Sum":
+	print(n1 + n2)
+elif type == "Substraction":
+	print(n1- n2)
+else
+	print("Operation not valid")
+end if
