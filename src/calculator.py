3f9f397afd7b9b6fac7e8d774dd00137d2462bc4
David Guerrero Prada <dguerreroprada@gmail.com>
2026-09-22 16:29:24 +0200

    Improve invalid operation message

--- src/calculator.py
+++ src/calculator.py
@@ -8,7 +8,7 @@ elif type == "Substraction":
 elif type == "Multiplication":
 	print(n1 * n2)
 else
-	print("Operation not valid")
+	print("Operation not supported")
 end if
 """Some comments here"""
 Another line of comments
